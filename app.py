3515444808d7d7fbbab854566de52efef6f8931e
lxy764139720 <764139720@qq.com>
2021-10-11 03:07:45 +0800

    完成GradCam++类激活图，并同时进行推断

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import os
 import sys
+import copy
 
 # Flask
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
-from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
 import torch
 from torchvision import transforms
+from torchvision.transforms.functional import to_pil_image
+from torchcam.cams import GradCAMpp
+from torchcam.utils import overlay_mask
 
 # Some utilities
-import numpy as np
+import matplotlib.pyplot as plt
 from util import base64_to_pil
 
 # Declare a flask app
 app = Flask(__name__)
 
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 classes = ['正常', '肺炎（非COVID-19）', '肺炎（COVID-19）']
 input_channel = 1
 input_size = (224, 224)
@@ -37,21 +41,19 @@ print('Model loading. Start serving...')
 model = torch.load(model_path, map_location='cpu')
 model.load_state_dict(torch.load(weight_path, map_location='cpu'))
 model.eval()
+model_cam = copy.deepcopy(model)
+cam_extractor = GradCAMpp(model_cam, input_shape=(1, 224, 224))
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(img, model):
     # Preprocessing the image
-    # x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    # x = np.array(img)
     x = to_tensor(img)
     x = torch.unsqueeze(x, 0)
     x = x.to(device)
 
     # Be careful how your trained model deals with the input
     # otherwise, it won't make correct prediction!
-    # x = preprocess_input(x, mode='tf')
 
     with torch.no_grad():
         output = model(x)
@@ -62,6 +64,26 @@ def model_predict(img, model):
     print(confidence, class_index)
     pred = classes[class_index]
     return pred, confidence.item()
+
+
+def grad_cam(img, model):
+    x = to_tensor(img)
+    x = torch.unsqueeze(x, 0)
+    x = x.to(device)
+
+    out = model(x)
+    activation_map = cam_extractor(out.squeeze(0).argmax().item(), out)
+    plt.imshow(activation_map.numpy())
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('/Users/lxy/PycharmProjects/mnist_web/activation_map.png')
+
+    img = img.convert('RGB')
+    result = overlay_mask(img, to_pil_image(activation_map, mode='F'), alpha=0.5)
+    plt.imshow(result)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('/Users/lxy/PycharmProjects/mnist_web/overlay.png')
 
 
 @app.route('/', methods=['GET'])
@@ -80,15 +102,10 @@ def predict():
         # img.save("./uploads/image.png")
 
         # Make prediction
+        print(img)
+        grad_cam(img, model_cam)
         pred, confidence = model_predict(img, model)
         print(pred, confidence)
-        # Process your result for human
-        # pred_proba = "{:.3f}".format(np.amax(pred))  # Max probability
-        # result = str(np.argmax(pred[0]))
-        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-        # result = str(pred_class[0][0][1])               # Convert to string
-        # result = result.replace('_', ' ').capitalize()
 
         # Serialize the result, you can add additional fields
         return jsonify(result=pred, probability=confidence)
